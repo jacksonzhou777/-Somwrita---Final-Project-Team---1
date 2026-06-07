d59b2f7ad66c37fe84e0cdeb1420ed74680c7526
sjin0817 <sjin0817@uni.sydney.edu.au>
2026-06-07 22:25:03 +1000

    Update audio-mechanic.js

--- Final_Ripple/audio-mechanic.js
+++ Final_Ripple/audio-mechanic.js
@@ -1,8 +1,32 @@
+let soundFile;
+let fft;
+let amplitude;
+let lastBassTrigger = 0;
+
 function setupAudioMechanic() {
+
+  soundFile = loadSound("music1.mp3");
+  fft = new p5.FFT(0.8, 1024);
+  amplitude = new p5.Amplitude();
 }
 
 function getAudioMechanicValue() {
-  return {
-    level: 0
-  };
+
+  if (!soundFile || !soundFile.isLoaded()) {
+    return {level: 0,bass: 0,treble: 0,shouldCreateRipple: false};
+  }
+
+  const level = amplitude.getLevel();
+  fft.analyze();
+  const bass = fft.getEnergy("bass");
+  const treble = fft.getEnergy("treble");
+
+  let shouldCreateRipple = false;
+
+  if (bass > 220 && millis() - lastBassTrigger > 200) {
+    shouldCreateRipple = true;
+    lastBassTrigger = millis();
+  }
+
+  return {level,bass,treble,shouldCreateRipple};
 }
